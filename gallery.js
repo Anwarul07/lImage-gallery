9bc13696bf094871871900016d854ae97e37c15c
Anwarul07 <mdanwarul064@gmail.com>
2024-05-07 00:39:09 +0530

    Upadate gallery.js

--- gallery.js
+++ gallery.js
@@ -1,1 +1,18 @@
-console.log("Image  gallery")
+console.log("Image  gallery");
+let scrol = document.querySelector(".gallery");
+let left = document.querySelector("#left");
+let right = document.querySelector("#right");
+scrol.addEventListener("wheel", (e) => {
+    e.preventDefault();
+    scrol.style.scrollBehavior= "smooth";
+    scrol.scrollLeft += e.deltaY;
+}); 
+left.addEventListener("click", ()=>{
+    scrol.style.scrollBehavior= "smooth";
+    scrol.scrollLeft+=900;
+});
+
+right.addEventListener("click", ()=>{
+    scrol.style.scrollBehavior= "smooth";
+    scrol.scrollLeft-=900;
+});
